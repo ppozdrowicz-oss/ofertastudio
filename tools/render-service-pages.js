873ef2992f96be9db1c00fb36c9826471f4c72b6
ppozdrowicz-oss <ppozdrowicz@gmail.com>
2026-06-13 21:57:18 +0200

    Add OfertaStudio favicon assets

--- tools/render-service-pages.js
+++ tools/render-service-pages.js
@@ -652,6 +652,12 @@ function render(page) {
 <meta name="twitter:title" content="${escapeHtml(page.title)}">
 <meta name="twitter:description" content="${escapeHtml(page.description)}">
 <meta name="twitter:image" content="${OG}">
+<meta name="theme-color" content="#0F172A">
+<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="manifest" href="/site.webmanifest">
 <script type="application/ld+json">
 ${schema}
 </script>
